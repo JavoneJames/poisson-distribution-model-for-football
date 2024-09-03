6d5df8823bc10c9e96f47aaa241ad0dd0df2e242
me <jovanekjames@icloud.com>
2024-09-03 14:29:04 +0100

    filter to remove unplayed games | extracted relevant data from json object | TO-DO write received data to file

--- server/fixtures.ts
+++ server/fixtures.ts
@@ -1,4 +1,4 @@
-async function fetchDataFromWeb(urls: Promise<Response>[]){
+export async function fetchDataFromWeb(urls: Promise<Response>[]){
   try {
     const responses: PromiseSettledResult<Response>[] = await Promise.allSettled(urls)
     for (const response of responses){
@@ -10,19 +10,35 @@ async function fetchDataFromWeb(urls: Promise<Response>[]){
     // ignore
   }
 }
-
 //TO-DO store rejected promises then output to log file
 function cacheRejectedPromises() {
   throw new Error("Function not implemented.");
 }
-
-//TO-DO check http response for fetch request
+//TO-DO output non-200 responses to log file
 function checkHttpResponse(fulfilledResponses: Response) {
-  if(fulfilledResponses.status !== 200|| !fulfilledResponses.headers.get("content-type")?.includes("application/json"))
+  if(fulfilledResponses.status !== 200 || !fulfilledResponses.headers.get("content-type")?.includes("application/json"))
     cacheRejectedPromises()
   tokenizeData(fulfilledResponses)
 }
-//TO-DO extract 'token' get the league and parse response body body text into json
-function tokenizeData(fulfilledResponses: Response) {
+async function tokenizeData(settledResponses: Response) {
+  const token: string = settledResponses.url.substring(settledResponses.url.lastIndexOf(`/`));
+  const fixtures: JSON = await settledResponses.json();
+  extractRelevantData(token, fixtures)
+}
+function extractRelevantData(token: string, fixtures: JSON) {
+  const storedExtractedData = Object.values(fixtures).filter((fixture) =>{
+    return ((fixture.HomeTeamScore && fixture.AwayTeamScore) !== null)
+  }).map((fixture)=>{
+    return {
+      HomeTeam: fixture.HomeTeam,
+      HomeTeamScore: fixture.HomeTeamScore, 
+      AwayTeam: fixture.AwayTeam, 
+      AwayTeamScore: fixture.AwayTeamScore
+    }
+  })
+  writeReceivedFixturesToFile(token, storedExtractedData)
+}
+//TO-DO implement func that handles writing extract data to file
+function writeReceivedFixturesToFile(token: string, storedExtractedData: { HomeTeam: any; HomeTeamScore: any; AwayTeam: any; AwayTeamScore: any; }[]) {
   throw new Error("Function not implemented.");
 }

bb328c603f5d2e7229b8da5125faaa983243af50
me <jovanekjames@icloud.com>
2024-09-12 19:50:05 +0100

    match results for a home win, away win or draw is checked TODO handle separate processing to create home and away standings

--- leagues/epl/epl.ts
+++ leagues/epl/epl.ts
@@ -1,6 +1,26 @@
 function readDataFromFile(): void {
-  const text: string = Deno.readTextFileSync(`${Deno.env.get("READ_EPL_2024")}`);
-  const parseToJSON: JSON = JSON.parse(text)
-  
+  const fileContent: string = Deno.readTextFileSync(`${Deno.env.get("READ_EPL_2024")}`);
+  const parseToJSON: JSON = JSON.parse(fileContent)
+  differentiateMatchResults(parseToJSON)
 }
+
+function differentiateMatchResults(data: JSON) {
+  Object.values(data).map((fixture)=>{
+    if(fixture.HomeTeamScore > fixture.AwayTeamScore)
+      homeStandings(fixture.HomeTeam)
+    else if (fixture.HomeTeamScore < fixture.AwayTeamScore)
+      awayStandings(fixture.AwayTeam)
+    else console.log("TO-DO add drawn games to home and away standings");
+    
+  })
+}
+
+function homeStandings(temp: string) {
+  console.log(`Home win ${temp}`);
+}
+
+function awayStandings(temp: string) {
+  console.log(`Away win ${temp}`);
+}
+
 readDataFromFile();

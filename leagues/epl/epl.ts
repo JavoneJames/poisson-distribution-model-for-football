667a99eba0ad5cb34bbbb4be7d92550a3a88fd20
me <jovanekjames@icloud.com>
2024-09-13 16:36:23 +0100

    implemented inserting and updating away standings and some minor overall refactoring

--- leagues/epl/epl.ts
+++ leagues/epl/epl.ts
@@ -9,8 +9,8 @@ function differentiateMatchResults(data: JSON) {
     if(fixture.HomeTeamScore > fixture.AwayTeamScore)
       homeStandings(fixture.HomeTeam, fixture.HomeTeamScore, fixture.AwayTeamScore)
     else if (fixture.HomeTeamScore < fixture.AwayTeamScore)
-      awayStandings(fixture.AwayTeam)
-    else return
+      awayStandings(fixture.AwayTeam, fixture.AwayTeamScore, fixture.HomeTeamScore)
+    //else return
   })
 }
 const storeHomeStanding: Map<string, object> = new Map();
@@ -18,14 +18,20 @@ const storeAwayStanding: Map<string, object> = new Map();
 
 function homeStandings(homeTeam: string, homeTeamScore: number, awayTeamScore: number) {
   if (!storeHomeStanding.has(homeTeam))
-    return storeHomeStanding.set(homeTeam,insertMatchResults(homeTeamScore, awayTeamScore));
-  return updateMatchResults(homeTeam, homeTeamScore, awayTeamScore);
+    storeHomeStanding.set(homeTeam,insertMatchResults(homeTeamScore, awayTeamScore));
+  else{
+    const results = Object(storeHomeStanding.get(homeTeam)).values();
+    updateMatchResults(results, homeTeamScore, awayTeamScore);
+  }
 }
 
-function updateMatchResults(teamName: string, homeTeamScore: number, awayTeamScore: number) {
-  const results = Object(storeHomeStanding.get(teamName)).values();
-  for (const result of results)
-    return result.GP += 1, result.W += 1, result.GF += homeTeamScore, result.GA += awayTeamScore, result.GD += homeTeamScore - awayTeamScore, result.Pts += 3;
+function awayStandings(awayTeam: string, awayTeamScore: number, homeTeamScore: number) {
+  if (!storeAwayStanding.has(awayTeam))
+    storeAwayStanding.set(awayTeam,insertMatchResults(awayTeamScore, homeTeamScore));
+  else{
+    const results = Object(storeAwayStanding.get(awayTeam)).values();
+    updateMatchResults(results, awayTeamScore, homeTeamScore);
+  }
 }
 
 function insertMatchResults(goalFor: number, goalAgainst: number): object {
@@ -43,9 +49,12 @@ function insertMatchResults(goalFor: number, goalAgainst: number): object {
   ];
 }
 
-function awayStandings(temp: string) {
-  //console.log(`Away win ${temp}`);
+// deno-lint-ignore no-explicit-any
+function updateMatchResults(results: any, homeTeamScore: number, awayTeamScore: number) {
+  for (const result of results)
+    return result.GP += 1, result.W += 1, result.GF += homeTeamScore, result.GA += awayTeamScore, result.GD += homeTeamScore - awayTeamScore, result.Pts += 3;
 }
 
 readDataFromFile();
 console.log(storeHomeStanding);
+console.log(storeAwayStanding);

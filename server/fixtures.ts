39c55cfc3f2ca62b67b06fb86e658df756ff5c12
me <jovanekjames@icloud.com>
2024-09-14 20:52:02 +0100

    minor code refactoring

--- server/fixtures.ts
+++ server/fixtures.ts
@@ -1,3 +1,5 @@
+import { temp } from "./datatypes.ts";
+
 const options = {
     dateStyle:"short",
     timeStyle: "medium"
@@ -6,16 +8,20 @@ const options = {
 export async function fetchDataFromWeb(urls: Promise<Response>[]): Promise<void>{
   try {
     const responses: PromiseSettledResult<Response>[] = await Promise.allSettled(urls)
-    for await(const response of responses)
+    for (const response of responses)
       if (response.status === "fulfilled")
         checkHttpResponse(response.value)
-  } catch (_) {
-    loggingHandler('FATAL unexpected error in func fetchDataFromWeb')
+  } catch (err) {
+    loggingHandler(`FATAL: unexpected error in fetchDataFromWeb - ${err.message}`);
   }
 }
 
 function loggingHandler(reason: string): void {
-  Deno.writeTextFileSync(`${Deno.env.get("logfile")}`, `${new Date().toLocaleString("en-GB", options)} ${reason}`, {create:true, append:true})
+  const timestamp = new Date().toLocaleString("en-GB", options)
+  const logMessage = `${timestamp} - ${reason}\n`
+  const logfilePath = Deno.env.get("logfile")
+  if (!logfilePath) throw new Error("File path not found in environment variable.")
+  Deno.writeTextFileSync(logfilePath, logMessage, { create: true, append: true })
 }
 
 function checkHttpResponse(fulfilledResponses: Response): void {
@@ -35,26 +41,22 @@ async function parseData(settledResponses: Response): Promise<void> {
   })
 }
 
-function extractRelevantData(token: string, fixtures: Promise<JSON>): void {
-  const storedExtractedData = Object.values(fixtures)
-  .filter((fixture)=>{
-    return ((fixture.HomeTeamScore && fixture.AwayTeamScore) !== null)
-  })
-  .map((fixture)=>{
-     return {
+function extractRelevantData(token: string, fixtures: any[]): void {
+  const extractedData = fixtures
+  .filter((fixture)=>((fixture.HomeTeamScore && fixture.AwayTeamScore) !== null))
+  .map((fixture)=>({
       HomeTeam: fixture.HomeTeam,
       HomeTeamScore: fixture.HomeTeamScore, 
       AwayTeam: fixture.AwayTeam, 
       AwayTeamScore: fixture.AwayTeamScore
-    }
-  })
-  writeReceivedFixturesToFile(token, storedExtractedData)
+  }))
+  writeReceivedFixturesToFile(token, extractedData)
 }
 
-async function writeReceivedFixturesToFile(token: string, storedExtractedData: { HomeTeam: string; HomeTeamScore: number; AwayTeam: string; AwayTeamScore: number; }[]): Promise<void> {
+async function writeReceivedFixturesToFile(token: string, extractedData: { HomeTeam: string; HomeTeamScore: number; AwayTeam: string; AwayTeamScore: number; }[]): Promise<void> {
   const filepath = getFilePath(token);
   using file = await Deno.open(filepath, {write:true, create:true})
-  const encoder = new TextEncoder().encode(JSON.stringify(storedExtractedData))
+  const encoder = new TextEncoder().encode(JSON.stringify(extractedData))
   const writer = file.writable.getWriter()
   await writer.write(encoder)
 }
@@ -69,3 +71,7 @@ function subStringTokenToDirName(token: string): { start: number; end: number; }
   const end = token.lastIndexOf("-");
   return { start, end };
 }
+
+fetchDataFromWeb([
+  fetch(`${Deno.env.get("FETCH_EPL_2024")}`, { signal: temp }),
+]);

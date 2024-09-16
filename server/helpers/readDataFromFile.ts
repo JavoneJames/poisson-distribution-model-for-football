d13f7de1d9dd2766cad30c5b7b03e62b754a63fe
me <jovanekjames@icloud.com>
2024-09-16 20:41:06 +0100

    refactored file structure and added comments

--- server/helpers/readDataFromFile.ts
+++ server/helpers/readDataFromFile.ts
@@ -2,58 +2,59 @@ import { loggingHandler } from "./loggingHandler.ts";
 import { LeagueData } from "../types/datatypes.d.ts";
 
 /**
- * Reads and parses fixture data from a specified file.
+ * Reads and parses fixture data from a specified file path.
  *
- * This function retrieves the file path from the environment variable
- * "READ_EPL_2024" and reads the content of the file. It then parses
- * the JSON content into an array of Fixture objects. If the file path
- * is not found in the environment variable or if there is an error
- * reading or parsing the file, it throws an appropriate error message.
- *
- * @returns An array of Fixture objects representing the match data.
- * @throws Will throw an error if the file path is not set in the environment
- *         variable or if there are issues reading or parsing the file.
+ * @param filePath - The file path to read the fixture data from.
+ * @returns An array of LeagueData representing the match data.
+ * @throws Will throw an error if the file path is invalid or if there are issues reading or parsing the file.
  */
-
-export function readDataFromFile(filePath: string): (LeagueData | null)[] {
+export function readDataFromFile(filePath: string): LeagueData[] {
   if (isValidPath(filePath)) {
-    loggingHandler(`File path not found in environment variable: ${filePath}`);
-    Deno.exit(1);
+    throw new Error(`File path not found in environment variable: ${filePath}`);
   }
   const arrayOfFilePaths = filePath.split(" ");
-  const dataCollection: (LeagueData | null)[] = processFiles(arrayOfFilePaths);
-  return dataCollection;
+  return arrayOfFilePaths.map(readAndParseFile);
 }
 
+/**
+ * Checks if the given file path is invalid.
+ *
+ * @param filePath - The file path to check.
+ * @returns True if the path is invalid, false otherwise.
+ */
 function isValidPath(filePath: string) {
   return typeof filePath !== "string" || filePath.trim() === "";
 }
 
-function processFiles(paths: string[]): (LeagueData | null)[] {
-  const fileContent: (LeagueData | null)[] = paths.map((path) =>
-    readAndParseFile(path)
-  );
-  return fileContent;
-}
-
-function readAndParseFile(path: string): LeagueData | null {
+/**
+ * Reads and parses a file, returning its content as LeagueData.
+ *
+ * @param path - The path to the file to read.
+ * @returns The parsed LeagueData or throws an error if reading fails.
+ */
+function readAndParseFile(path: string): LeagueData {
   try {
     const fileContent = Deno.readTextFileSync(path);
     const data = JSON.parse(fileContent) as LeagueData;
 
     if (isEmpty(data)) {
-      loggingHandler(`Data is empty for file: ${path}`);
-      return null;
+      throw new Error(`Data is empty for file: ${path}`);
     }
     return data;
   } catch (error) {
     loggingHandler(
       `Failed to read or parse file: ${path}. Error: ${error.message}`,
     );
-    return null;
+    throw error; // Rethrow the error for upstream handling.
   }
 }
 
+/**
+ * Checks if the given data structure is empty.
+ *
+ * @param data - The data structure to check.
+ * @returns True if the data structure is empty, false otherwise.
+ */
 const isEmpty = (
   data: { [s: string]: unknown } | ArrayLike<unknown>,
 ): boolean => {

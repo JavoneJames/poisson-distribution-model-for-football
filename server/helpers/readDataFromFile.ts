56a8a17e565147e14e658ab7ed2b76ede8add115
me <jovanekjames@icloud.com>
2024-09-16 12:57:13 +0100

    breaking change - modified readDataFromFile to be able to multiple files <home and away standing> to construct home and away analysis - need to fix break in generateHomeAwayStanding file

--- server/helpers/readDataFromFile.ts
+++ server/helpers/readDataFromFile.ts
@@ -1,35 +1,52 @@
 import { loggingHandler } from "./loggingHandler.ts";
-import {LeagueData } from "../types/datatypes.d.ts";
+import { LeagueData } from "../types/datatypes.d.ts";
 
 /**
  * Reads and parses fixture data from a specified file.
- * 
- * This function retrieves the file path from the environment variable 
- * "READ_EPL_2024" and reads the content of the file. It then parses 
- * the JSON content into an array of Fixture objects. If the file path 
- * is not found in the environment variable or if there is an error 
+ *
+ * This function retrieves the file path from the environment variable
+ * "READ_EPL_2024" and reads the content of the file. It then parses
+ * the JSON content into an array of Fixture objects. If the file path
+ * is not found in the environment variable or if there is an error
  * reading or parsing the file, it throws an appropriate error message.
- * 
+ *
  * @returns An array of Fixture objects representing the match data.
- * @throws Will throw an error if the file path is not set in the environment 
+ * @throws Will throw an error if the file path is not set in the environment
  *         variable or if there are issues reading or parsing the file.
  */
-export function readDataFromFile(filePath: string): LeagueData {
-  
-  if (!filePath) throw new Error("File path not found in environment variable.");
-  try {
-    const fileContent = Deno.readTextFileSync(filePath);
-    const data = JSON.parse(fileContent) as LeagueData;
-    if (isEmpty(data)) {
-      throw new Error("Parsed data is empty.");
-    }
-    return data
-  } catch (err) {
-    loggingHandler(`Failed to read or parse file: ${err.message}`);
-    Deno.exit(1)
+export function readDataFromFile(filePath: string): LeagueData[] {
+  if (!filePath) {
+    throw new Error("File path not found in environment variable.");
   }
+
+  const arrayOfFilePaths = filePath.split(" ");
+  const dataCollection: LeagueData[] = [];
+  
+  for (const path of arrayOfFilePaths) {
+   
+    try {
+      const fileContent = Deno.readTextFileSync(path);
+      const data = JSON.parse(fileContent) as LeagueData;
+
+      if (isEmpty(data)) {
+        loggingHandler(`data is empty for file: ${path}`);
+        continue; // Move to the next file if the data is empty
+      }
+      dataCollection.push(data);
+    } catch (err) {
+      loggingHandler(
+        `Failed to read or parse file: ${path}. Error: ${err.message}`,
+      );
+      // Don't exit here, move to the next path
+    }
+  }
+  if(dataCollection.length === 0)
+    throw new Error("Failed to read or parse data from all provided file paths.");
+  return dataCollection
 }
 
-const isEmpty = (data: { [s: string]: unknown; } | ArrayLike<unknown>): boolean => {
+const isEmpty = (
+  data: { [s: string]: unknown } | ArrayLike<unknown>,
+): boolean => {
   return Object.keys(data).length === 0;
 };
--- server/helpers/writeDataToFile.ts
+++ server/helpers/writeDataToFile.ts
@@ -1,4 +1,4 @@
-import { Standing } from "../types/datatypes.d.ts";
+import { HomeAwayStanding } from "../types/datatypes.d.ts";
 
 type ExtractedObject = {
   [league: string]: {
@@ -9,7 +9,7 @@ type ExtractedObject = {
   }[];
 };
 
-async function writeDataToFile(filepath: string, data: Map<string, Standing> | ExtractedObject): Promise<void> {
+async function writeDataToFile(filepath: string, data: Map<string, HomeAwayStanding> | ExtractedObject): Promise<void> {
   using file = await Deno.open(filepath, { write: true, create: true });
   const serializedData: string = serializeData(data);
   const encoder = new TextEncoder().encode(serializedData);
@@ -17,7 +17,7 @@ async function writeDataToFile(filepath: string, data: Map<string, Standing> | E
   await writer.write(encoder);
 }
 
-function serializeData(data: Map<string, Standing> | ExtractedObject) {
+function serializeData(data: Map<string, HomeAwayStanding> | ExtractedObject) {
   if (data instanceof Map)
     return JSON.stringify(Array.from(data.entries()));
   return JSON.stringify(data);
@@ -32,7 +32,7 @@ function getFilePathFromToken(token: string): string {
   return `./server/data/${token}.json`;
 }
 
-export async function writeLocalData(league: string | null, filename: string, data: Map<string, Standing>): Promise<void> {
+export async function writeLocalData(league: string | null, filename: string, data: Map<string, HomeAwayStanding>): Promise<void> {
   const filepath = getLocalFilePath(league, filename);
   await writeDataToFile(filepath, data);
 }

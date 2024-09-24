d28930aca9bd3f732c5e575ca88afcf8b4c315e1
me <jovanekjames@icloud.com>
2024-09-24 12:20:55 +0100

    refactor types: added Leagues type, changed ExtractedFixture to Fixture, added countedTeams to LeagueStats, and improved formatting.

--- server/types/datatypes.d.ts
+++ server/types/datatypes.d.ts
@@ -1,59 +1,62 @@
-// Type representing a collection of leagues, where each league is associated with an array of promises 
-// that resolve to HTTP responses.
-export type Leagues = {
-  league: Promise<Response>[]
-}
-
 // Type representing the parsed JSON structure for data fetched from the web regarding match details.
 export type ParsedJsonFromWeb = {
-  MatchNumber: number                // The unique number identifying the match.
-  RoundNumber: number                // The round in which the match is played.
-  DateUtc: string                    // The date and time of the match in UTC format.
-  Location: string                   // The location where the match is held.
-  Group: null                        // The group to which the match belongs (currently set to null).
-  HomeTeam: string                   // The name of the home team.
-  AwayTeam: string                   // The name of the away team.
-  HomeTeamScore: number | null       // The score of the home team (nullable if not available).
-  AwayTeamScore: number | null       // The score of the away team (nullable if not available).
+  MatchNumber: number; // The unique number identifying the match.
+  RoundNumber: number; // The round in which the match is played.
+  DateUtc: string; // The date and time of the match in UTC format.
+  Location: string; // The location where the match is held.
+  Group: null; // The group to which the match belongs (currently set to null).
+  HomeTeam: string; // The name of the home team.
+  AwayTeam: string; // The name of the away team.
+  HomeTeamScore: number | null; // The score of the home team (nullable if not available).
+  AwayTeamScore: number | null; // The score of the away team (nullable if not available).
 }[];
 
-// Type representing a fixture that contains details about a specific match between two teams.
-type Fixture = {
-  HomeTeam: string                   
-  AwayTeam: string                   
-  HomeTeamScore: number              
-  AwayTeamScore: number             
-}
+export type AllLeagueStandings = LeagueData[];
 
 // Define the type for the league object with Fixture as its signature
 export type LeagueData = {
-  [leagueKey: string]: Fixture[];
-}
+  [leagueKey: string]: ExtractedFixture[];
+};
+
+// Type representing a fixture that contains details about a specific match between two teams.
+type ExtractedFixture = {
+  HomeTeam: string;
+  HomeTeamScore: number;
+  AwayTeam: string;
+  AwayTeamScore: number;
+};
 
 // Type representing the standings of a team in the league.
 export type HomeAwayStanding = {
-  GP: number  
-  W: number   
-  D: number   
-  L: number   
-  GF: number  
-  GA: number  
-  GD: number  
-  Pts: number 
-}
+  GP: number;
+  W: number;
+  D: number;
+  L: number;
+  GF: number;
+  GA: number;
+  GD: number;
+  Pts: number;
+};
 
 export type HomeAwayAnalysis = {
-  AHG: number                        //Average Home Goals
-  AHCG: number                       //Average Home Conceded Goals
-  AS: number                         //Attack Strength
-  DS: number                         //Defensive Strength
-}
+  AHG: number; //Average Home Goals
+  AHCG: number; //Average Home Conceded Goals
+  AS: number; //Attack Strength
+  DS: number; //Defensive Strength
+};
 
 // Type representing the possible outcomes of a match.
-export type Outcome = 'win' | 'loss' | 'draw'
+export type Outcome = "win" | "loss" | "draw";
 
 export type LeagueStats = {
   totalGF: number;
   totalGA: number;
-  countedTeams: number;
 };
+
+// Type for a team entry
+export type TeamEntry = {
+  [leagueKey: string]: HomeAwayStanding[];
+};
+
+// Type for the standings, which is an array of team entries
+export type Standings = TeamEntry[];
